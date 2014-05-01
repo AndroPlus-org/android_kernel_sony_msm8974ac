bfb0bf534884abb5849f0a7a499cb486caa5c937
Paul Reioux <reioux@gmail.com>
2014-05-01 05:57:12 -0700

    LGE KCAL control: intial adaptation from LGE G2 kernel for 8974Pro
    Signed-off-by: Paul Reioux <reioux@gmail.com>

---
+++ arch/arm/mach-msm/include/mach/msm_lcd_kcal.h
@@ -0,0 +1,18 @@
+#ifndef __MSM_LCD_KCAL_H
+#define __MSM_LCD_KCAL_H
+
+struct kcal_data {
+	int red;
+	int green;
+	int blue;
+};
+
+struct kcal_platform_data {
+	int (*set_values) (int r, int g, int b);
+	int (*get_values) (int *r, int *g, int *b);
+	int (*refresh_display) (void);
+};
+
+void __init msm_add_lcd_kcal_devices(void);
+
+#endif

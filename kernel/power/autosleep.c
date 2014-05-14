7f4293fccbf3d906c32189bc0f811139baeef5b7
DooMLoRD <metoo.mememe@gmail.com>
2014-05-15 02:18:16 +0530

    kernel/power/powersuspend: remove userspace dependency from powersuspend (part 2)

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -12,6 +12,10 @@
 #include <linux/pm_wakeup.h>
 #include <linux/wakelock.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "power.h"
 
 /*
@@ -151,8 +155,14 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
-	} else {
-		pm_wakep_autosleep_enabled(false);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
+ 	} else {
+ 		pm_wakep_autosleep_enabled(false);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);

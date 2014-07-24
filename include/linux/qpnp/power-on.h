1abb075dbafb6fd2949131d9316134481ddbb9f3
cocafe <cocafehj@gmail.com>
2014-07-24 20:38:55 +0800

    qpnp-pon: Add POnKey Emulator
    I hope that my power-on key will last longer.
    
    Also, preparing API for S2W stuff..
    
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- include/linux/qpnp/power-on.h
+++ include/linux/qpnp/power-on.h
@@ -53,6 +53,10 @@ int qpnp_pon_system_pwr_off(enum pon_power_off_type type);
 int qpnp_pon_is_warm_reset(void);
 int qpnp_pon_trigger_config(enum pon_trigger_source pon_src, bool enable);
 int qpnp_pon_wd_config(bool enable);
+bool qpnp_pon_is_initialized(void);
+int qpnp_pon_dvdd_reset(void);
+int qpnp_get_pmic_version(void);
+void qpnp_ponkey_emulate(int press);
 #else
 static int qpnp_pon_system_pwr_off(enum pon_power_off_type type)
 {

8196ea26ebb6b50023bec468f06c29479581f5cc
cocafe <cocafehj@gmail.com>
2014-07-25 20:51:08 +0800

    misc: Stop spamming kmsg
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- lib/ratelimit.c
+++ lib/ratelimit.c
@@ -47,7 +47,7 @@ int ___ratelimit(struct ratelimit_state *rs, const char *func)
 
 	if (time_is_before_jiffies(rs->begin + rs->interval)) {
 		if (rs->missed)
-			printk(KERN_WARNING "%s: %d callbacks suppressed\n",
+			pr_debug("%s: %d callbacks suppressed\n",
 				func, rs->missed);
 		rs->begin   = 0;
 		rs->printed = 0;

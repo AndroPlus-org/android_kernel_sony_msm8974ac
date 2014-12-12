da1aa10156c970868ddf626517722348cb6b3521
Myself5 <myself5@carbonrom.org>
2014-12-12 22:31:34 +0100

    Add nr_running stats for fast_mpdecision

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -112,7 +112,7 @@ void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
 DEFINE_MUTEX(sched_domains_mutex);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 
-#ifdef CONFIG_INTELLI_PLUG
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_FAST_HOTPLUG)
 DEFINE_PER_CPU_SHARED_ALIGNED(struct nr_stats_s, runqueue_stats);
 #endif
 
@@ -2206,7 +2206,7 @@ unsigned long this_cpu_load(void)
 	return this->cpu_load[0];
 }
 
-#ifdef CONFIG_INTELLI_PLUG
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_FAST_HOTPLUG)
 unsigned long avg_nr_running(void)
 {
 	unsigned long i, sum = 0;

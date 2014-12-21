8242ad83609714538b768a964d1d1779c9109eb0
WedyDQ10 <hosigumayuugi@gmail.com>
2014-12-21 15:48:57 +0900

    Revert "Fast Charge: ported to Xperia Z2"
    This reverts commit ceeefdaad067d1c2687b568962c095a0b761b6ba.

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -995,14 +995,7 @@ qpnp_chg_idcmax_set(struct qpnp_chg_chip *chip, int mA)
 			chip->dc_chgpth_base + CHGR_I_MAX_REG, 1);
 	}
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-	if (force_fast_charge >= 1))
-		dc = fast_charge_level / QPNP_CHG_I_MAXSTEP_MA;
-	else
-		dc = mA / QPNP_CHG_I_MAXSTEP_MA;
-#else
- 	dc = mA / QPNP_CHG_I_MAXSTEP_MA;
-#endif
+	dc = mA / QPNP_CHG_I_MAXSTEP_MA;
 
 	pr_debug("current=%d setting 0x%x\n", mA, dc);
 	rc = qpnp_chg_write(chip, &dc,
@@ -1053,10 +1046,6 @@ qpnp_chg_iusb_trim_set(struct qpnp_chg_chip *chip, int trim)
 	return rc;
 }
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-#include <linux/fastchg.h>
-#endif
-
 static int
 qpnp_chg_iusbmax_set(struct qpnp_chg_chip *chip, int mA)
 {
@@ -1081,20 +1070,8 @@ qpnp_chg_iusbmax_set(struct qpnp_chg_chip *chip, int mA)
 	}
 
 	/* Impose input current limit */
-#ifdef CONFIG_FORCE_FAST_CHARGE
-	if (force_fast_charge >= 1)
-		mA = fast_charge_level;
-		if (mA > FAST_CHARGE_900)
-			mA = FAST_CHARGE_900;
-	else {
-		if (chip->maxinput_usb_ma)
-			mA = (chip->maxinput_usb_ma) <=
-				mA ? chip->maxinput_usb_ma : mA;
-	}
-#else
- 	if (chip->maxinput_usb_ma)
- 		mA = (chip->maxinput_usb_ma) <= mA ? chip->maxinput_usb_ma : mA;
-#endif
+	if (chip->maxinput_usb_ma)
+		mA = (chip->maxinput_usb_ma) <= mA ? chip->maxinput_usb_ma : mA;
 
 	usb_reg = mA / QPNP_CHG_I_MAXSTEP_MA;
 
@@ -5257,22 +5234,8 @@ qpnp_chg_reduce_power_stage(struct qpnp_chg_chip *chip)
 	bool vchg_loop = get_prop_vchg_loop(chip);
 	bool ichg_loop = qpnp_chg_is_ichg_loop_active(chip);
 	bool usb_present = qpnp_chg_is_usb_chg_plugged_in(chip);
-#ifdef CONFIG_FORCE_FAST_CHARGE
-	bool usb_ma_above_wall;
-#else
- 	bool usb_ma_above_wall =
- 		(qpnp_chg_usb_iusbmax_get(chip) > USB_WALL_THRESHOLD_MA);
-#endif
-
-#ifdef CONFIG_FORCE_FAST_CHARGE
-	if (force_fast_charge >= 1) {
-		if (fast_charge_level <= FAST_CHARGE_900)
-			usb_ma_above_wall = false;
-		else
-			usb_ma_above_wall = true;
-	} else
-		qpnp_chg_usb_iusbmax_get(chip) > USB_WALL_THRESHOLD_MA;
-#endif
+	bool usb_ma_above_wall =
+		(qpnp_chg_usb_iusbmax_get(chip) > USB_WALL_THRESHOLD_MA);
 
 	if (fast_chg
 		&& usb_present

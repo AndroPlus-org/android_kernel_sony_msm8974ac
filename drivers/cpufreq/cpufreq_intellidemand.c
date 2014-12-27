4d2bb7859cf0fc6a9edf7efa871ea2db64a9cb7c
WedyDQ10 <hosigumayuugi@gmail.com>
2014-12-27 11:33:49 +0900

    Fixed compiling error for intellidemand

--- drivers/cpufreq/cpufreq_intellidemand.c
+++ drivers/cpufreq/cpufreq_intellidemand.c
@@ -1308,7 +1308,7 @@ void set_two_phase_freq_by_cpu ( int cpu_nr, int cpufreq){
 	two_phase_freq_array[cpu_nr-1] = cpufreq;
 }
 
-int input_event_boosted(void)
+int input_event_boostedd(void)
 {
 	unsigned long flags;
 
@@ -1691,7 +1691,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		}
 	}
 
-	if (input_event_boosted()) {
+	if (input_event_boostedd()) {
 		return;
 	}
 
@@ -1818,7 +1818,7 @@ static void do_dbs_timer(struct work_struct *work)
 				delay -= jiffies % delay;
 		}
 	} else {
-		if (input_event_boosted())
+		if (input_event_boostedd())
 			goto sched_wait;
 
 		__cpufreq_driver_target(dbs_info->cur_policy,

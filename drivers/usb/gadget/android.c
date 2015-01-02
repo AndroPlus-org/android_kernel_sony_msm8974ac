bf73d95c67c7a51daf519395afb2f09e4f767e47
WedyDQ10 <hosigumayuugi@gmail.com>
2015-01-02 11:52:23 +0900

    Revert "Fixed merge durp of USB keyboard/mouse"
    This reverts commit 39f85553ef32d8b1ddfd399fd48ac443cbb6b40c.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2793,8 +2793,6 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 						name);
 			}
 		}
-		/* HID driver always enabled, it's the whole point of this kernel patch */
-		android_enable_function(dev, conf, "hid");
 	}
 
 	/* Free uneeded configurations if exists */

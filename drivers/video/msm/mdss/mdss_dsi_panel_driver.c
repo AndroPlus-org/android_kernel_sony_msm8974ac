df029f2b9336d16deb2e0ec03631321828eb34cb
WedyDQ10 <hosigumayuugi@gmail.com>
2015-01-08 11:48:36 +0900

    Revert "Revert "mdss_dsi_panel_driver : Pannel to Panel.""
    This reverts commit 92d0d443a0848b6cbb1ee40a74704a695eb11f5a.

--- drivers/video/msm/mdss/mdss_dsi_panel_driver.c
+++ drivers/video/msm/mdss/mdss_dsi_panel_driver.c
@@ -987,7 +987,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	}
 
 #ifdef CONFIG_POWERSUSPEND
-	set_power_suspend_state_pannel_hook(POWER_SUSPEND_INACTIVE);
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -1116,7 +1116,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 			spec_pdata->new_vfp;
 
 #ifdef CONFIG_POWERSUSPEND
-	set_power_suspend_state_pannel_hook(POWER_SUSPEND_ACTIVE);
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
 	vs_handle.vsync_handler = (mdp_vsync_handler_t)vsync_handler;
 	vs_handle.cmd_post_flush = false;

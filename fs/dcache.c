2d94ff2a5b3ea6030ad198fe842559209f9cf14e
WedyDQ10 <hosigumayuugi@gmail.com>
2015-01-16 20:22:32 +0900

    Revert "fs/dcache.c: Fix the too small buffer for dname"
    This reverts commit b91c4fdb4f7f234a36901f0034e21d5e9e3ec74f.

--- fs/dcache.c
+++ fs/dcache.c
@@ -2715,7 +2715,7 @@ char *dynamic_dname(struct dentry *dentry, char *buffer, int buflen,
 			const char *fmt, ...)
 {
 	va_list args;
-	char temp[256];
+	char temp[64];
 	int sz;
 
 	va_start(args, fmt);

630ba0cf259ac9fc6b982b461cced65922b50050
WedyDQ10 <hosigumayuugi@gmail.com>
2015-01-16 20:27:44 +0900

    Revert "readahead: make context readahead more conservative"
    This reverts commit 42db78fbba4ed73be87595b3fb4d7194ee84bd96.

--- mm/readahead.c
+++ mm/readahead.c
@@ -374,10 +374,10 @@ static int try_context_readahead(struct address_space *mapping,
 	size = count_history_pages(mapping, ra, offset, max);
 
 	/*
-	 * not enough history pages:
+	 * no history pages:
 	 * it could be a random read
 	 */
-	if (size <= req_size)
+	if (!size)
 		return 0;
 
 	/*
@@ -388,8 +388,8 @@ static int try_context_readahead(struct address_space *mapping,
 		size *= 2;
 
 	ra->start = offset;
-	ra->size = min(size + req_size, max);
-	ra->async_size = 1;
+	ra->size = get_init_ra_size(size + req_size, max);
+	ra->async_size = ra->size;
 
 	return 1;
 }

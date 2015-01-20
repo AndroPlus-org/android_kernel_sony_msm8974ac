5cd683630018a721993ca7aedd0d6631a24e3fec
WedyDQ10 <hosigumayuugi@gmail.com>
2015-01-20 22:34:43 +0900

    Changed params for smartassH3

--- drivers/cpufreq/cpufreq_smartassH3.c
+++ drivers/cpufreq/cpufreq_smartassH3.c
@@ -47,7 +47,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ (768*1000)
+#define DEFAULT_AWAKE_IDEAL_FREQ (729*1000)
 static unsigned int awake_ideal_freq;
 
 /*
@@ -56,7 +56,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ (384*1000)
+#define DEFAULT_SLEEP_IDEAL_FREQ (300*1000)
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -64,7 +64,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP (810*1000)
+#define DEFAULT_RAMP_UP_STEP (729*1000)
 static unsigned int ramp_up_step;
 
 /*
@@ -72,7 +72,7 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP (384*1000)
+#define DEFAULT_RAMP_DOWN_STEP (300*1000)
 static unsigned int ramp_down_step;
 
 /*

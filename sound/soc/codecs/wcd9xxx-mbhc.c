aa9a01bc032217cf8af309b47485ff90ae21aefe
Linux Build Service Account <lnxbuild@localhost>
2015-02-04 22:33:14 -0800

    Merge "ASoC: wcd9xxx: Do not recalibrate DCE_Z in the middle of mic voltage measurements"

--- sound/soc/codecs/wcd9xxx-mbhc.c
+++ sound/soc/codecs/wcd9xxx-mbhc.c
@@ -1316,9 +1316,6 @@ static void wcd9xxx_recalibrate(struct wcd9xxx_mbhc *mbhc,
 			snd_soc_write(mbhc->codec, WCD9XXX_A_CDC_MBHC_B1_CTL,
 				      reg);
 			if (dce_z) {
-				pr_debug("%s: dce_nsc_cs_z 0x%x -> 0x%x\n",
-					 __func__, mbhc->mbhc_data.dce_nsc_cs_z,
-					 dce_z & 0xffff);
 				mbhc->mbhc_data.dce_nsc_cs_z = dce_z;
 				/* update v_cs_ins_h with new dce_nsc_cs_z */
 				mbhc->mbhc_data.v_cs_ins_h = wcd9xxx_codec_v_sta_dce(
@@ -1980,6 +1977,8 @@ wcd9xxx_codec_get_plug_type(struct wcd9xxx_mbhc *mbhc, bool highhph)
 		if (rt[i].swap_gnd)
 			wcd9xxx_codec_hphr_gnd_switch(codec, false);
 	}
+	/* recalibrate DCE/STA GND voltages */
+	wcd9xxx_recalibrate(mbhc, &mbhc->mbhc_bias_regs, false);
 
 	/* recalibrate DCE/STA GND voltages */
 	wcd9xxx_recalibrate(mbhc, &mbhc->mbhc_bias_regs, false);

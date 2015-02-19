72592f58bdfc749259d397e1020c03d824792cd6
WedyDQ10 <hosigumayuugi@gmail.com>
2015-02-19 09:57:19 +0900

    Fixed elementalx

--- drivers/cpufreq/cpufreq_elementalx.c
+++ drivers/cpufreq/cpufreq_elementalx.c
@@ -710,18 +710,6 @@ static void dbs_freq_increase(struct cpufreq_policy *p, unsigned load, unsigned 
 			CPUFREQ_RELATION_L : CPUFREQ_RELATION_H);
 }
 
-int set_two_phase_freq(int cpufreq)
-{
-	int i  = 0;
-	for ( i = 0 ; i < NR_CPUS; i++)
-		two_phase_freq_array[i] = cpufreq;
-	return 0;
-}
-
-void set_two_phase_freq_by_cpu ( int cpu_nr, int cpufreq){
-	two_phase_freq_array[cpu_nr-1] = cpufreq;
-}
-
 int input_event_boosted(void)
 {
 	unsigned long flags;
@@ -1175,10 +1163,6 @@ static struct input_handler dbs_input_handler = {
 	.id_table	= dbs_ids,
 };
 
-
-void set_input_event_min_freq_by_cpu ( int cpu_nr, int cpufreq){
-	input_event_min_freq_array[cpu_nr-1] = cpufreq;
-}
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				   unsigned int event)
 {

c194f0a41b0b14f1676cfc2c5bd6bedd38b87891
WedyDQ10 <hosigumayuugi@gmail.com>
2015-03-12 12:42:05 +0900

    cpufreq: smartmax: added new governor based on ondemand and smartass2

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -423,6 +423,9 @@ extern struct cpufreq_governor cpufreq_gov_HYPER;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartmax)
 #endif
 
 

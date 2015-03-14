0ddfdb24ea08573c8bd78808b171faa8a4bff1f0
WedyDQ10 <hosigumayuugi@gmail.com>
2015-03-14 16:32:59 +0900

    Updated alucard governor

--- drivers/cpufreq/cpufreq_alucard.c
+++ drivers/cpufreq/cpufreq_alucard.c
@@ -82,6 +82,40 @@ struct cpufreq_alucard_cpuinfo {
 	struct mutex timer_mutex;
 };
 
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
+{
+	u64 idle_time;
+	u64 cur_wall_time;
+	u64 busy_time;
+
+	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+
+	busy_time = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+
+	idle_time = cur_wall_time - busy_time;
+	if (wall)
+		*wall = cputime_to_usecs(cur_wall_time);
+
+	return cputime_to_usecs(idle_time);
+}
+
+static u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy)
+{
+	u64 idle_time = get_cpu_idle_time_us(cpu, io_busy ? wall : NULL);
+
+	if (idle_time == -1ULL)
+		return get_cpu_idle_time_jiffy(cpu, wall);
+	else if (!io_busy)
+		idle_time += get_cpu_iowait_time_us(cpu, wall);
+
+	return idle_time;
+}
+
 static DEFINE_PER_CPU(struct cpufreq_alucard_cpuinfo, od_alucard_cpuinfo);
 
 static unsigned int alucard_enable;	/* number of CPUs using this policy */
@@ -586,7 +620,7 @@ static void do_alucard_timer(struct work_struct *work)
 	if (delay <= 0)
 		delay = usecs_to_jiffies(MIN_SAMPLING_RATE);
 
-	mod_delayed_work_on(cpu, system_wq, &alucard_cpuinfo->work, delay);
+	queue_delayed_work_on(cpu, system_wq, &alucard_cpuinfo->work, delay);
 
 	mutex_unlock(&alucard_cpuinfo->timer_mutex);
 }
@@ -654,9 +688,9 @@ static int cpufreq_governor_alucard(struct cpufreq_policy *policy,
 		if (delay <= 0)
 			delay = usecs_to_jiffies(MIN_SAMPLING_RATE);
 
-		INIT_DEFERRABLE_WORK(&this_alucard_cpuinfo->work, do_alucard_timer);
+		INIT_DELAYED_WORK_DEFERRABLE(&this_alucard_cpuinfo->work, do_alucard_timer);
 
-		mod_delayed_work_on(this_alucard_cpuinfo->cpu, system_wq, &this_alucard_cpuinfo->work, delay);
+		queue_delayed_work_on(this_alucard_cpuinfo->cpu, system_wq, &this_alucard_cpuinfo->work, delay);
 
 		break;
 

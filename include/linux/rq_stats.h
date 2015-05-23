1a2082b992b0d4f5ca875f1cc70da89a0dc91346
WedyDQ10 <hosigumayuugi@gmail.com>
2015-05-23 15:54:42 +0900

    Updated rq_stats to mach Dorimanx one

--- include/linux/rq_stats.h
+++ include/linux/rq_stats.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011,2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -20,6 +20,8 @@ struct rq_data {
 	unsigned long def_timer_last_jiffy;
 	unsigned int def_interval;
 	unsigned int hotplug_disabled;
+	unsigned int hotplug_enabled;
+	unsigned int bricked_hotplug_enabled;
 	int64_t def_start_time;
 	struct attribute_group *attr_group;
 	struct kobject *kobj;

a49477067d73eeb466c8bb04490edf5efa0b9bfe
WedyDQ10 <hosigumayuugi@gmail.com>
2015-06-18 21:46:20 +0900

    GCC 5.0 warning fix

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -57,12 +57,12 @@ void *return_address(unsigned int level)
 }
 
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
-
+/*
 void *return_address(unsigned int level)
 {
 	return NULL;
 }
-
+*/
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
 
 EXPORT_SYMBOL_GPL(return_address);

6b004466adf686b27f6ee7a69390b0e42ee05136
anarkia1976 <stefano.villa1976@gmail.com>
2015-06-30 09:31:11 +0200

    msm8974pro: regulator: switch from warn to info for voltage debug

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -661,7 +661,7 @@ ssize_t store_UV_mV_table(struct cpufreq_policy *policy, char *buf,
 		ret = sscanf(buf, "%s", size_cur);
 		buf += strlen(size_cur) + 1;
 	}
-	pr_warn("faux123: user voltage table modified!\n");
+	pr_info("krait: regulator: user voltage table modified!\n");
 
 	return count;
 }

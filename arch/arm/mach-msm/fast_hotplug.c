521a9f1f01711a3bbf6494975897ee7dac6ce599
WedyDQ10 <hosigumayuugi@gmail.com>
2015-07-20 17:16:44 +0900

    Fixed fast_hotplug warning

--- arch/arm/mach-msm/fast_hotplug.c
+++ arch/arm/mach-msm/fast_hotplug.c
@@ -43,20 +43,20 @@
 #define SCREEN_OFF_SINGLECORE		1
 
 
-#define PLUG_IN_CORE_1_THRESHOLD	2800
-#define PLUG_IN_CORE_2_THRESHOLD	6000
-#define PLUG_IN_CORE_3_THRESHOLD	7500
+#define PLUG_IN_CORE_1_THRESHOLD	2500
+#define PLUG_IN_CORE_2_THRESHOLD	4000
+#define PLUG_IN_CORE_3_THRESHOLD	4500
 
 #define PLUG_IN_CORE_1_DELAY		1
-#define PLUG_IN_CORE_2_DELAY		4
-#define PLUG_IN_CORE_3_DELAY		4
+#define PLUG_IN_CORE_2_DELAY		3
+#define PLUG_IN_CORE_3_DELAY		3
 
 #define PLUG_OUT_CORE_1_THRESHOLD	2500
-#define PLUG_OUT_CORE_2_THRESHOLD	4000
-#define PLUG_OUT_CORE_3_THRESHOLD	6000
+#define PLUG_OUT_CORE_2_THRESHOLD	3000
+#define PLUG_OUT_CORE_3_THRESHOLD	3500
 
-#define PLUG_OUT_CORE_1_DELAY		2
-#define PLUG_OUT_CORE_2_DELAY		1
+#define PLUG_OUT_CORE_1_DELAY		3
+#define PLUG_OUT_CORE_2_DELAY		2
 #define PLUG_OUT_CORE_3_DELAY		1
 
 static int fast_hotplug_enabled = FAST_HOTPLUG_ENABLED;
@@ -196,17 +196,19 @@ static int get_slowest_cpu(void){
 }
 
 static void plug_in(int online_cpu_count){
-	int i;
+	int cpu;
 
 	if(online_cpu_count >= max_cpu_on)
 		return;
 
 	mutex_lock(&mutex);
 	singlecore = false;
-	for(i = (CONFIG_NR_CPUS - 1); i > 0; i--){
-		if(!cpu_online(i)){
-			cpu_up(i);
-			break;
+	for_each_possible_cpu(cpu){
+		if(cpu != 0){
+			if(!cpu_online(cpu)){
+				cpu_up(cpu);
+				break;
+			}
 		}
 	}
 	mutex_unlock(&mutex);
@@ -386,6 +388,7 @@ static struct input_handler hotplug_input_handler = {
 static int enable_fast_hotplug(const char *val, const struct kernel_param *kp){
 	int cpu;
 	int ret = param_set_bool(val, kp);
+	int rc;
 	
 	if(!fast_hotplug_enabled){
 		pr_info(HOTPLUG_INFO_TAG"Fast hotplug disabled\n");
@@ -411,7 +414,6 @@ static int enable_fast_hotplug(const char *val, const struct kernel_param *kp){
 
 		flush_workqueue(hotplug_wq);
 		
-		int rc;
 		rc = input_register_handler(&hotplug_input_handler);
 		init_timer(&unboost_timer);
 
@@ -492,3 +494,4 @@ static int __init hotplug_init(void)
 
 
 late_initcall(hotplug_init);
+

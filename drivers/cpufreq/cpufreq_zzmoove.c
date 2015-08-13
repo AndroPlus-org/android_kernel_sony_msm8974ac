7915ced5f6b17633be3c09c48f4c6e6e897437cb
Myself5 <myself5@carbonrom.org>
2015-08-13 11:47:42 -0400

    Revert "zzmoove: disable the USE_LCD_NOTFIER macro"
    This reverts commit d160333ac901a95a47b040a973800d1c8cf4b0f5.

--- drivers/cpufreq/cpufreq_zzmoove.c
+++ drivers/cpufreq/cpufreq_zzmoove.c
@@ -32,7 +32,7 @@
 // #define DISABLE_POWER_MANAGEMENT
 
 // AP: use msm8974 lcd status notifier
-// #define USE_LCD_NOTIFIER
+#define USE_LCD_NOTIFIER
 
 #include <linux/cpu.h>
 #ifdef USE_LCD_NOTIFIER

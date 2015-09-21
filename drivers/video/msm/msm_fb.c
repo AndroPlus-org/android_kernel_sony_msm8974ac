82c05711ac9ebec1f19ac234c7cdd168c535ba4d
WedyDQ10 <hosigumayuugi@gmail.com>
2015-09-21 21:43:16 +0900

    Reduce log spam

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -90,10 +90,10 @@ static struct ion_client *iclient;
 
 u32 msm_fb_debug_enabled;
 /* Setting msm_fb_msg_level to 8 prints out ALL messages */
-u32 msm_fb_msg_level = 7;
+u32 msm_fb_msg_level = 0;
 
 /* Setting mddi_msg_level to 8 prints out ALL messages */
-u32 mddi_msg_level = 5;
+u32 mddi_msg_level = 0;
 
 extern int32 mdp_block_power_cnt[MDP_MAX_BLOCK];
 extern unsigned long mdp_timer_duration;
